52c1b16faae785c59de459bceacdcae94ba3345b
Igor Znamensky <igor.znamensky@gmail.com>
2022-05-04 11:07:00 +0300

    04 Serving static files

--- app.js
+++ app.js
@@ -6,14 +6,28 @@ const PORT = 4000
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-app.get('/', (req, res, next) => {
+// Middleware
+const { requireLogin } = require("./middleware/login.middleware");
+
+
+// Routes
+const loginRoute = require('./routes/login.routes')
+
+// Mount routes
+app.use('/login', loginRoute)
+
+
+// Root path
+app.get('/', requireLogin, (req, res, next) => {
     let payload = {
         pageTitle: "Home page title"
     }
+
     res.status(200).render("home", payload)
 })
 
 
+// Start server
 const server = app.listen(PORT, function () {
     console.log(`Server running on ${PORT}`)
     console.log(`http://localhost:${PORT}`)

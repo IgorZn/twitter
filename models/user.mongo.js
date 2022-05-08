4f1d58a79573ee6f2612403c836fa176f5fc3276
Igor Znamensky <igor.znamensky@gmail.com>
2022-05-08 14:37:09 +0300

    04 Sessions

--- models/user.mongo.js
+++ models/user.mongo.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
@@ -35,5 +36,15 @@ const UserSchema = new mongoose.Schema({
     { timestamps: true }
 );
 
+
+// Encrypt user password
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+})
 
 module.exports = mongoose.model('User', UserSchema);

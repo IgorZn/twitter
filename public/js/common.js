07dd0bccf981243ecefe378387ab9ca0667d8e52
Igor Znamensky <igor.znamensky@gmail.com>
2022-05-18 11:44:21 +0300

    07 Adding a click handler to the post button

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,12 @@
-$("#postTextarea").keyup( event => {
+$("#postTextarea").on("keyup",event => {
     let textbox = $(event.target);
     let value = textbox.val().trim()
-    console.log(value);
+
+    let submitButton = $("#submitPostButton");
+
+    if (submitButton.length == 0) return alert("No POST button");
+
+    if (!value) return submitButton.prop("disabled", true);
+
+    submitButton.prop("disabled", false)
 })

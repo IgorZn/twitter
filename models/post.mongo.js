c8abce424de7947ffa9306a5f799a788e5c9d164
Igor Znamensky <igor.znamensky@gmail.com>
2022-05-22 09:21:25 +0300

    07 Inserting the post into the database

---
+++ models/post.mongo.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const PostSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        trim: true
+    },
+    postedBy: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    },
+    pinned: Boolean
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);

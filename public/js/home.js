b11e96d7d1537cf7edc794cce7cd1d28225f88e5
Igor Znamensky <igor.znamensky@gmail.com>
2022-06-11 10:17:54 +0300

    08 Populating the posted by data

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,18 @@
 $(window).on( "load", () => {
     $.get("/api/posts", results => {
-        console.log(results)
-
+        outputPost(results, $(".postContainer"))
     });
 });
+
+function outputPost(results, container) {
+    container.html("");
+
+    results.forEach( result => {
+        let html = createPostHtml(result);
+        container.append(html)
+    });
+
+    if (!results){
+        container.append("<span class='noResults'>Nothing to show.</span>")
+    }
+}
